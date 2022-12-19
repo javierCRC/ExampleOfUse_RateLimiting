92f0a74b1454e0f3681c64a039a6e4194ffe3d26
Javier Eduardo Mendoza Blandon <javier.mendoza@samtec.com>
2022-12-18 22:14:49 -0600

    Version 01

---
+++ ExampleOfUse_RateLimiting/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace ExampleOfUse_RateLimiting.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [EnableRateLimiting("fixedWindows")] // this is the configuration of rate limiting for all endpoint of this controller
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+    }
+}
---
+++ ExampleOfUse_RateLimiting/Modules/RateLimiter/RateLimiterExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.RateLimiting; // middleware provides rate limiting middleware. Apps configure rate limiting policies and then attach the policies to endpoints.
+
+namespace ExampleOfUse_RateLimiting.Modules.RateLimiter
+{
+    public static class RateLimiterExtensions
+    {
+        public static IServiceCollection AddRateLimiting(this IServiceCollection services, IConfiguration configuration)
+        {
+            var vFixedWindowsPolicy = "fixedWindows";
+
+            services.AddRateLimiter(configureOptions => // to add a rate limiting service to the service collection
+            {
+                configureOptions.AddFixedWindowLimiter(policyName: vFixedWindowsPolicy, fixedWindow => // to create a fixed window limiter with a policy name of "fixed" and sets:
+                {
+                    fixedWindow.PermitLimit = int.Parse(configuration["RateLimiting:vPermitLimit"]); // A maximum of 5 requests
+                    fixedWindow.Window = TimeSpan.FromSeconds(int.Parse(configuration["RateLimiting:vWindow"])); //  per each 30-second window are allowed.
+                    fixedWindow.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+                    fixedWindow.QueueLimit = int.Parse(configuration["RateLimiting:vQueueLimit"]); // Maximum cumulative permit count of queued acquisition requests.
+
+                });
+
+                // YOU CAN ADD MORE policy here
+
+                configureOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests; // The HTTP 429 Too Many Requests response status code indicates the user has sent too many requests in a given amount of time ("rate limiting").
+            });
+
+            return services;
+        }
+    }
+}
+
+// This is the official documentation https://learn.microsoft.com/en-us/aspnet/core/performance/rate-limit?view=aspnetcore-7.0
